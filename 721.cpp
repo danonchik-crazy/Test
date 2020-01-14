7094bf5a8aa0df0164195e9f343d60d2dfa21339
Daniel Khromov <danielkhromov@gmail.com>
2020-01-14 22:11:28 +0300

    Upadate 2

--- 721.cpp
+++ 721.cpp
@@ -3,8 +3,7 @@ using namespace std;
 
 int main()
 {
-int a, b, l, N ;
-cin>>a>>b>>l>>N;
+
             int s = 0;
             s = a + ((N - 1) * 2 * a) + b * (N-1) * 2 + l*2;
             cout<<s;
